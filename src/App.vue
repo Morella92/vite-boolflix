2da5e002c04b5ad55499adb56f0bcfec048fb35d
Morella92 <morena.piem@outlook.com>
2023-03-07 18:30:23 +0100

    REFACTORY call in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,64 @@
-<script >
-  import axios from 'axios'
-  import store from './store'
-  import Header from './components/Header.vue' 
+<template>
+  <div class="container">
+    <Header @search="fetchMovies" />
+    <div v-if="movies.length > 0 || series.length > 0">
+      <h2>Film:</h2>
+      <ul>
+        <li v-for="movie in movies" :key="movie.id">
+          <h3>{{ movie.title }}</h3>
+          <p>Titolo originale: {{ movie.original_title }}</p>
+          <p>Lingua: {{ movie.original_language }}</p>
+          <p>Voto: {{ movie.vote_average }}</p>
+        </li>
+      </ul>
+  
+      <h2>Serie TV:</h2>
+      <ul>
+        <li v-for="serie in series" :key="serie.id">
+          <h3>{{ serie.name }}</h3>
+          <p>Titolo originale: {{ serie.original_name }}</p>
+          <p>Lingua: {{ serie.original_language }}</p>
+          <p>Voto: {{ serie.vote_average }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+  import axios from 'axios';
 
   export default {
-
-    components:{
+    components: {
       Header
     },
-    data(){
-      return{
-        store
+    data() {
+      return {
+        movies: [],
+        series: [],
       }
     },
-    methods:{
-      fetchMovies(){
-
-        const movies = this.store.movies
-        const series = this.store.series
-        const userSearch = this.store.userSearch
-
-        //faccio la chiamata solo se l'input viene compilato
-        if(userSearch !== ''){
-
-          //ricerco il film
+    methods: {
+      fetchMovies(userSearch) {
+        if (userSearch !== '') {
           axios
-          .get('https://api.themoviedb.org/3/search/movie?api_key=ba0d841780bc45e2866779bbc2a8e4b2&query=m&language=it-IT')
-
-          .then((res) =>{
-              movies = res.data.results
+          .get(`https://api.themoviedb.org/3/search/movie?api_key=ba0d841780bc45e2866779bbc2a8e4b2&query=${userSearch}&language=it-IT`)
+          .then((res) => {
+            this.movies = res.data.results
           })
 
-          //ricerco la serie
           axios
-          .get('https://api.themoviedb.org/3/search/tv?api_key=ba0d841780bc45e2866779bbc2a8e4b2&query=m&language=it-IT')
-
-          .then((res)=>{
-            series= res.data.results
+          .get(`https://api.themoviedb.org/3/search/tv?api_key=ba0d841780bc45e2866779bbc2a8e4b2&query=${userSearch}&language=it-IT`)
+          .then((res) => {
+            this.series = res.data.results
           })
         }
       }
-
-    },
-
-    created(){
-      this.fetchMovies()
     }
-    
   }
 </script>
 
-<template>
-  <div>
-   <Header/>
-  </div>
- 
-</template>
 
 <style lang="scss">
   @use './style/general.scss'
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,26 @@
+<template>
+   <div class="search-bar">
+        <h1 class="header-logo">boolfix</h1>
+        <input class="header-input" type="text" v-model="userSearch" placeholder="Cerca il film o la serie tv">
+        <button class="header-button" @click="searchClicked"> Cerca </button>
+    </div>
+</template>
+  
 <script>
-    import store from '../store'
-
     export default {
-
-        components:{
-            
-        },
-
-        data(){
-            return{
-                store
-            }
-        },
-        
+      data() {
+        return {
+          userSearch: ''
+        }
+      },
+      methods: {
+        searchClicked() {
+          this.$emit('search', this.userSearch)
+        }
+      }
     }
 </script>
-
-<template>
-    <header class="header">
-        <div class="container">
-            <div class="search-bar">
-                <input class="header-input" type="text" v-model="userSearch" placeholder="Cerca il film o la serie tv">
-                <button class="header-button"> Cerca </button>
-            </div>
-
-            <ul v-for="movie in movies" :key="movie.id">
-                <li>
-                    <h2>Film:</h2>
-                    <h2>{{ movie.title }}</h2>
-                    <h3>{{ movie.original_title }}</h3>
-                    <p>{{ movie.original_language }}</p>
-                    <p>{{ movie.vote_average }}</p>
-                </li>
-            </ul>
-        </div>  
-    </header>
-</template>
+  
 
 <style lang="scss" scoped>
      .header-input{
