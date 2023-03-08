61c41524e8a799a7d6c6121e55af3a1426e33eaf
Morella92 <morena.piem@outlook.com>
2023-03-08 17:07:05 +0100

    FEAT navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
    <div class="flex-container">
         <h1 class="header-logo">boolflix</h1>
+        <ul v-for="link in links" class="link-style">
+          <li>
+            <a href="#">{{ link }}</a>
+          </li>
+        </ul>
         <div class="search-bar">
-            <input class="header-input" type="text" v-model="userSearch" @keyup.enter="searchClicked" placeholder="Cerca il film o la serie tv">
-            <button class="header-button" @click="searchClicked"> Cerca </button>
+          <input class="header-input" type="text" v-model="userSearch" @keyup.enter="searchClicked" placeholder="Cerca il film o la serie tv">
+          <button class="header-button" @click="searchClicked"> Cerca </button>
         </div>
     </div>
 </template>
@@ -12,7 +17,15 @@
     export default {
       data() {
         return {
-          userSearch: ''
+          userSearch: '',
+          links: [
+            'Home',
+            'Serie TV',
+            'Film',
+            'Originali',
+            'Aggiunti di recente',
+            'La mia lista'
+          ]
         }
       },
       methods: {
@@ -31,13 +44,22 @@
         align-items: center;
         padding-top: 15px;
     }
+
+    .link-style{
+      color: white;
+      font-size: 15px;
+
+      &:hover{
+      color: rgb(112, 107, 107);
+    }
+    }
+
     .header-logo{
         color: red;
         text-transform: uppercase;
     }
 
      .header-input{
-        
         padding: 10px;
     }
 
@@ -46,5 +68,9 @@
         margin-left: 20px;
         background-color: rgb(105, 99, 99);
         color: white;
+        
+        &:hover{
+          transform: scale(1.2);
+        }
     }
 </style>
